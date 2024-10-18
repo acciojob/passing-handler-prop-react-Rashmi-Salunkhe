f3b77d255f5f4292a70c38194a87dd33133cf8f0
Rashmi-Salunkhe <158038788+Rashmi-Salunkhe@users.noreply.github.com>
2024-10-18 11:32:45 +0530

    Update Selection.js

--- src/components/Selection.js
+++ src/components/Selection.js
@@ -1,1 +1,35 @@
 
+import React, { useState } from 'react';
+
+const Selection = (props) => {
+  const [background, setBackground] = useState('');
+
+  const { applyColor, colorId } = props;
+
+  const handleClick = () => {
+    applyColor(setBackground);
+  };
+
+  const boxStyle = {
+    width: '100px',
+    height: '100px',
+    border: '1px solid #ccc',
+    margin: '10px',
+    display: 'inline-block',
+    backgroundColor: background,
+    cursor: 'pointer'
+  };
+
+  return (
+    <div 
+      style={boxStyle}
+      className="fix-box"  // Add the class name "fix-box"
+      onClick={handleClick}
+      data-testid={colorId}  // Add data-testid attribute
+    >
+      {/* Content inside the box, if any */}
+    </div>
+  );
+}
+
+export default Selection;
